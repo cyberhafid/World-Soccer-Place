72e6f304d91a88f85c0b41eccd0621d0d13d8650
cyberhafid <47986136+cyberhafid@users.noreply.github.com>
2019-04-04 12:05:19 +0200

    a cause de doudou
    sdfds

--- src/App.js
+++ src/App.js
@@ -6,10 +6,8 @@ import Carousel1 from "./components/header/carousel/carousel"
 import Menuv from "./components/menu/menubar"
 import Tableaut from "./components/menu/tableau"
 
-import { library } from './assets/fonts/font@fortawesome/fontawesome-svg-core';
+import { library } from './assets/fonts/@fortawesome/fontawesome-svg-core';
 import { faEnvelope, faTruck, faKey, faBomb } from './assets/fonts/@fortawesome/free-solid-svg-icons';
-
-
 
 library.add(faEnvelope, faTruck, faBomb,  faKey);
 
