6e14987f99cd22fa01a4b693524179818c18ca86
hafid <cyberhafid@gmail.com>
2019-04-04 13:57:58 +0200

    valide rox

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Container, Row, Col } from 'reactstrap';
 import './App.css';
 import MyNavbar from './components/navbar/navbar';
 import Progressbarre from "./components/header/progressbarre/progressbarre"
@@ -6,16 +7,10 @@ import Carousel1 from "./components/header/carousel/carousel"
 import Menuv from "./components/menu/menubar"
 import Tableaut from "./components/menu/tableau"
 
+
 import { library } from './assets/fonts/@fortawesome/fontawesome-svg-core';
-
 import { faEnvelope, faTruck, faKey, faBomb } from './assets/fonts/@fortawesome/free-solid-svg-icons';
-
-
-
-
-
-library.add(faEnvelope, faTruck, faBomb,  faKey);
-
+library.add(faEnvelope, faTruck, faBomb, faKey);
 
 
 
@@ -23,17 +18,43 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <MyNavbar />
-        <Carousel1 />
 
-        
+
+
+
         <div className="container">
-          <Progressbarre />
-          <Menuv />
-          <Tableaut />
+          <div Container>
+
+            <Row>
+              <Col> <MyNavbar /></Col>
+            </Row>
+
+
+            <Row>
+              <Col>  <Carousel1 /></Col>
+            </Row>
+
+            <Row>
+              <Col><Progressbarre /></Col>
+            </Row>
 
 
 
+            <Row>
+              <Col sm="3">   <Menuv /></Col>
+              <Col sm="6"> <Tableaut /></Col>
+              <Col sm="6">3 of 3</Col>
+            </Row>
+
+
+
+
+
+
+
+
+
+          </div>
         </div>
       </div>
     );
