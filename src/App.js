9a285f7290a68e657f27e9f2a60cff91c09df3de
alexlucas <alex.plomberie83@gmail.com>
2019-04-10 17:33:01 +0200

    first commit LongBook

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import ProgressBar from "./components/header/progressbar/progress"
 import Carousel1 from "./components/header/carousel/carousel"
 import MyTable from './components/contents/tables/tables_container';
 import MyFooter from './components/footer/containerFooter';
-import Book from './components/contents/bookmaker/bookmaker'
+import Book from './components/contents/bookmaker/bookmaker';
+import LongBook from './components/contents/LongBook/longBook';
+
 import { Row, Col } from "reactstrap"
 import Matchencours from "./components/contents/menu/menubar"
 import Matchresults from "./components/contents/menu/menubar2"
@@ -20,16 +22,13 @@ class App extends Component {
         <div className="container-fluid backg">
           <ProgressBar />
           <Row>
-            <Col sm="3">
-              <Matchencours />
-              <Matchresults />
-              <Matchupcomming />
+            <Col sm="2">
+            
             </Col>
-            <Col sm="6">
-              <MyTable />
+            <Col sm="8">
+            <LongBook />
           </Col>
-            <Col sm="3">
-              <Book />
+            <Col sm="2">
             </Col>
           </Row>
           <MyFooter />
