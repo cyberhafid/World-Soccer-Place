609e4f747efc48fa735bf3563e8fb7a3bc045945
alexlucas <alex.plomberie83@gmail.com>
2019-04-11 16:37:19 +0200

    finalize error LongBookmaker

--- src/components/contents/LongBook/longBook.jsx
+++ src/components/contents/LongBook/longBook.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Table } from 'reactstrap';
 import { Button } from 'reactstrap';
 import './longBook.css'; 
-
 export default class LongBook extends React.Component {
   render() {
     return (
@@ -15,42 +14,37 @@ export default class LongBook extends React.Component {
             <th><strong>2</strong></th>
             <th className="bonusOffered"><strong>Bonus offered</strong></th>
           </tr>
-        </thead>
-        
+        </thead>        
         <tbody>
           <tr>
-            <th scope="row"><img width="55%" src="https://www.itiforums.com/beeiti/images/logos_societes/pmu.png" alt="Card image cap" /></th>
+            <th scope="row"><img width="55%" src="https://www.itiforums.com/beeiti/images/logos_societes/pmu.png" alt="Card longBookmaker" /></th>
             <td><Button color="secondary">cotes</Button>{' '}</td>
             <td><Button color="secondary">cotes</Button>{' '}</td>
             <td><Button color="secondary">cotes</Button>{' '}</td>
             <td><Button color="warning"className="buttonBook2">To register</Button></td>
           </tr>
           <tr>
-            <th scope="row"><img width="55%" src="https://avantagebet.com/wp-content/uploads/2018/04/logo-betclic-1.jpg" alt="Card image cap" /></th>
+            <th scope="row"><img width="55%" src="https://avantagebet.com/wp-content/uploads/2018/04/logo-betclic-1.jpg" alt="Card longBookmaker" /></th>
             <td><Button color="secondary">cotes</Button>{' '}</td>
             <td><Button color="secondary">cotes</Button>{' '}</td>
             <td><Button color="secondary">cotes</Button>{' '}</td>
             <td><Button color="warning"className="buttonBook2">To register</Button></td>
           </tr>
           <tr>
-            <th scope="row"><img width="55%" src="https://y4f9x2y5.stackpathcdn.com/wp-content/uploads/2019/01/NetBet.png" alt="Card image cap" /></th>
+            <th scope="row"><img width="55%" src="https://y4f9x2y5.stackpathcdn.com/wp-content/uploads/2019/01/NetBet.png" alt="Card longBookmaker" /></th>
             <td><Button color="secondary">cotes</Button>{' '}</td>
             <td><Button color="secondary">cotes</Button>{' '}</td>
             <td><Button color="secondary">cotes</Button>{' '}</td>
             <td><Button color="warning"className="buttonBook2">To register</Button></td>
           </tr>
           <tr>
-            <th scope="row"><img width="55%" src="https://www.wannabet.fr/wp-content/uploads/2017/08/zebet-300x133.png" alt="Card image cap" /></th>
+            <th scope="row"><img width="55%" src="https://www.wannabet.fr/wp-content/uploads/2017/08/zebet-300x133.png" alt="Card longBookmaker" /></th>
             <td><Button color="secondary">cotes</Button>{' '}</td>
             <td><Button color="secondary">cotes</Button>{' '}</td>
             <td><Button color="secondary">cotes</Button>{' '}</td>
             <td><Button color="warning"className="buttonBook2">To register</Button></td>
-          </tr>
-          
-
-
-        </tbody>
-      
+          </tr>          
+        </tbody>      
         </Table>
     );
   }
