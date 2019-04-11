36503098e24c4787925bb45757a4a5f3bc55e2a1
Radoudou <Gtonepiece13@gmail.com>
2019-04-11 16:34:03 +0200

    initaliaze the vs banner

---
+++ src/LogoClub.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react'
+
+export default function LogoClub({url, className}) {
+  return (
+    <div className={className}>
+      <img src={url} width="100%" height="100%" alt="url"/>
+    </div>
+  )
+}
