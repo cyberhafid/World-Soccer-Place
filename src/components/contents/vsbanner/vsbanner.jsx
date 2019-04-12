b43168046b1f7238fa3ee4921dfb3c046b594e7e
Radoudou <Gtonepiece13@gmail.com>
2019-04-12 09:40:22 +0200

    logo in vs banner

---
+++ src/components/contents/vsbanner/vsbanner.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { Container, } from 'reactstrap';
+import LogoClub from './logoclub/LogoClub';
+import './vsbanner.scss';
+
+export default class VsBanner extends Component {
+    render() {
+        return (
+            <Container className="css-banner">
+                <h2>Match</h2>
+                <div className="soccer-place">
+                    <LogoClub className="position-logo" url="http://assets.stickpng.com/thumbs/580b57fcd9996e24bc43c4d2.png" />
+                    <LogoClub className="position-logo" url="https://png.pngtree.com/element_our/md/20180427/md_5ae2bfcc6832a.png" />
+                    <LogoClub className="position-logo" url="http://assets.stickpng.com/thumbs/580b57fcd9996e24bc43c4d8.png" />
+                </div>
+            </Container >
+        );
+    }
+}
--- src/pages/Bet.jsx
+++ src/pages/Bet.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import VsBanner from '../components/contents/vsbanner/vsbanner';
 
 export default class Bet extends Component {
   render() {
     return (
       <div className="container-fluid backg">
+        <VsBanner />
       </div>
     );
   }
